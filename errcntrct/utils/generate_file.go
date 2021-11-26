80d75fe381838e6cd72f0d3731aa1017c9da42dd
Muhammad Furqon Haq <saucon.mfh@gmail.com>
2021-11-27 01:19:01 +0700

    Update generate_file.go

--- errcntrct/utils/generate_file.go
+++ errcntrct/utils/generate_file.go
@@ -26,8 +26,8 @@ func TemplateJSONtoGolangConst(source string, output string, packagename string)
 	}
 	defer f.Close()
 
-	_, err = f.WriteString(`// This file is generated using errcntrct tool.\n
-	//Check out for more info "https://github.com/Saucon/errcntrct"`+ 
+	_, err = f.WriteString(`// This file is generated using errcntrct tool. \n
+	// Check out for more info "https://github.com/Saucon/errcntrct"`+ 
 			       "\n" +
 	"package " + packagename + "\n\n\n" +
 	"const (\n")
